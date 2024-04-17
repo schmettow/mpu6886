19411e9e723720e79f1c23fc56b2aa74172d45c2
schmettow <schmettow@web.de>
2024-04-17 17:06:20 +0200

    removed garbage

--- target/debug/examples/simple-8ec18a2bee8cd714.d
+++
@@ -1,5 +0,0 @@
-/home/martin/embededOS/rust_embedded/esp32/test/mpu6886/target/debug/examples/simple-8ec18a2bee8cd714.rmeta: examples/simple.rs
-
-/home/martin/embededOS/rust_embedded/esp32/test/mpu6886/target/debug/examples/simple-8ec18a2bee8cd714.d: examples/simple.rs
-
-examples/simple.rs:
